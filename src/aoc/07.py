c0fc15bc34a16661b9cdf2b8c807dc0f04027ae7
Daniel Tosone (KAI DSC) <daniel.tosone@k-ai.at>
2024-12-07 13:26:09 +0100

    🎄 Day 07 - 1st part

---
+++ src/aoc/07.py
@@ -0,0 +1,66 @@
+"""Day 6: Guard Gallivant."""
+
+from pathlib import Path
+
+import typer
+from loguru import logger
+
+from aoc import InstanceType, read_day_instance_lines
+
+
+def first_part(instance: InstanceType, day: str) -> int:
+    """First part of day 6."""
+    lines = read_day_instance_lines(instance, day)
+    equations = parse_equations(lines)
+    sum_results = 0
+    for result, terms in equations:
+        if is_valid(result, terms[1:], terms[0]):
+            sum_results += result
+
+    return sum_results
+
+
+def parse_equations(lines: list[str]) -> list[tuple[int, list[int]]]:
+    """Parse equations."""
+    equations = []
+    for line in lines:
+        result_str, terms_str = line.split(": ")
+        result = int(result_str)
+        terms = [int(t) for t in terms_str.split(" ")]
+        equations.append((result, terms))
+
+    return equations
+
+
+def is_valid(result: int, terms: list[int], cumulative_value: int) -> bool:
+    """Build combinations."""
+    if cumulative_value > result:
+        return False
+    if len(terms) == 0:
+        return cumulative_value == result
+    else:
+        is_valid_plus = is_valid(result, terms[1:], cumulative_value + terms[0])
+        is_valid_times = is_valid(result, terms[1:], cumulative_value * terms[0])
+        return is_valid_plus or is_valid_times
+
+
+def second_part(instance: InstanceType, day: str) -> int:
+    """Second part of day 6."""
+    lines = read_day_instance_lines(instance, day)  # noqa: F841
+    count = 0
+    return count
+
+
+def main(instance: InstanceType) -> None:
+    """Main function."""
+    day = Path(__file__).name.removesuffix(".py")
+
+    result = first_part(instance, day)
+    logger.info(f"1st:\t{result}")
+
+    result = second_part(instance, day)
+    logger.info(f"2nd:\t{result}")
+
+
+if __name__ == "__main__":
+    typer.run(main)
