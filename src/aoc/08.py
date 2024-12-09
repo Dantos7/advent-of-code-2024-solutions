96cbe9bdc5bb285ee09617038ae40a4b4cb00508
Dantos7 <daniel.tosone@gmail.com>
2024-12-09 11:45:32 +0100

    🎄 Day 08 - 2nd part

--- src/aoc/08.py
+++ src/aoc/08.py
@@ -76,9 +76,58 @@ def get_antinodes_couple(
 
 def second_part(instance: InstanceType, day: str) -> int:
     """Second part of day 8."""
-    matrix = read_day_instance_matrix(instance, day)  # noqa: F841
-    count = 0
-    return count
+    matrix = read_day_instance_matrix(instance, day)
+    frequencies_locations: dict[str, list[tuple[int, int]]] = get_frequencies_locations(matrix)
+    antinodes = set()
+    max_i = len(matrix) - 1
+    max_j = len(matrix[0]) - 1
+    for locations in frequencies_locations.values():
+        antinodes_freq = get_antinodes_locations_line(locations, max_i, max_j)
+        antinodes.update(antinodes_freq)
+    return len(antinodes)
+
+
+def get_antinodes_locations_line(locations: list[tuple[int, int]], max_i: int, max_j: int) -> set[tuple[int, int]]:
+    """Get antinodes locations (line)."""
+    all_antinodes = set()
+    for i in range(len(locations)):
+        for j in range(i + 1, len(locations)):
+            antinodes_couple_line = get_antinodes_couple_line(locations[i], locations[j], max_i, max_j)
+            all_antinodes.update(antinodes_couple_line)
+
+    return all_antinodes
+
+
+def get_antinodes_couple_line(
+    loc_1: tuple[int, int],
+    loc_2: tuple[int, int],
+    max_i: int,
+    max_j: int,
+) -> set[tuple[int, int]]:
+    """Get antinodes for the given couple of locations (line)."""
+    antinodes_line = set()
+    antinodes_line.add(loc_1)
+    antinodes_line.add(loc_2)
+
+    i_diff = loc_2[0] - loc_1[0]
+    j_diff = loc_2[1] - loc_1[1]
+
+    antinode_i_1 = loc_1[0] - i_diff
+    antinode_j_1 = loc_1[1] - j_diff
+
+    while 0 <= antinode_i_1 <= max_i and 0 <= antinode_j_1 <= max_j:
+        antinodes_line.add((antinode_i_1, antinode_j_1))
+        antinode_i_1 -= i_diff
+        antinode_j_1 -= j_diff
+
+    antinode_i_2 = loc_2[0] + i_diff
+    antinode_j_2 = loc_2[1] + j_diff
+    while 0 <= antinode_i_2 <= max_i and 0 <= antinode_j_2 <= max_j:
+        antinodes_line.add((antinode_i_2, antinode_j_2))
+        antinode_i_2 += i_diff
+        antinode_j_2 += j_diff
+
+    return antinodes_line
 
 
 def main(instance: InstanceType) -> None:
