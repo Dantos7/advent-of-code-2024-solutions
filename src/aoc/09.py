c43a4ad360961418a46aee772c329c409b15167a
Dantos7 <daniel.tosone@gmail.com>
2024-12-09 11:45:40 +0100

    🎄 Day 09 - 2nd part

--- src/aoc/09.py
+++ src/aoc/09.py
@@ -26,16 +26,16 @@ def convert_to_explicit(line_list: list[str]) -> list[str]:
     for c in line_list:
         repetitions = int(c)
         if file:
-            explicit_line += [str(file_id)]*repetitions
+            explicit_line += [str(file_id)] * repetitions
             file = False
             file_id += 1
         else:
-            explicit_line += ["."]*repetitions
+            explicit_line += ["."] * repetitions
             file = True
     return explicit_line
 
 
-def move_blocks_from_right(explicit_line:list[str]) -> list[str]:
+def move_blocks_from_right(explicit_line: list[str]) -> list[str]:
     """Move blocks from right to left."""
     i = 0
     j = len(explicit_line) - 1
@@ -65,8 +65,62 @@ def compute_checksum(rearranged_line: list[str]) -> int:
 def second_part(instance: InstanceType, day: str) -> int:
     """Second part of day 9."""
     line = read_day_instance_lines(instance, day)[0]
+    line_list = list(line)
+    explicit_line = convert_to_explicit(line_list)
+    rearranged_line = move_files_from_right(explicit_line)
+    checksum = compute_checksum(rearranged_line)
+    return checksum
 
-    return 0
+
+def move_files_from_right(explicit_line: list[str]) -> list[str]:
+    """Move files from right to left."""
+    i = 0
+    j = len(explicit_line) - 1
+    already_moved = set()
+    while i < j:
+        # Move j pointer to the first non-empty space
+        while explicit_line[j] == "." and i < j:
+            j -= 1
+        j_size = 0
+        j_id = explicit_line[j]
+        while explicit_line[j - j_size] == j_id:
+            j_size += 1
+        if j_id in already_moved:
+            j -= j_size
+            continue
+
+        # Move i pointer to the first empty space
+        while explicit_line[i] != "." and i < j:
+            i += 1
+
+        # Try every possible size of empty space
+        i_trial = i
+        while i_trial < j:
+            # Compute size of empty space
+            i_size = 0
+            while explicit_line[i_trial + i_size] == ".":
+                i_size += 1
+
+            if explicit_line[i_trial] == "." and explicit_line[j] != "." and i_trial != j and i_size >= j_size:
+                for k in range(j_size):
+                    explicit_line[i_trial + k] = explicit_line[j - k]
+                    explicit_line[j - k] = "."
+                i_trial += j_size
+                already_moved.add(j_id)
+                break
+            if j_id in already_moved:
+                break
+
+            i_trial += i_size + 1
+            # Move i_trial pointer to the next empty space
+            while explicit_line[i_trial] != "." and i_trial < j:
+                i_trial += 1
+
+        j -= j_size
+        i = 0
+
+    return explicit_line
+
 
 def main(instance: InstanceType) -> None:
     """Main function."""
