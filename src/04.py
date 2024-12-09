7ede9c6e866e1a7cda30d45187d8f7a978c0a935
Dantos7 <daniel.tosone@gmail.com>
2024-12-09 11:44:49 +0100

    🎄 Day 04 - 1st part

---
+++ src/04.py
@@ -0,0 +1,100 @@
+from enum import Enum
+import typer
+import os
+
+class InstanceType(str, Enum):
+    EXAMPLES = "examples"
+    INPUTS = "inputs"
+
+
+def first_part(instance: InstanceType, day: str):
+    with open(f"data/{instance.value}/{day}.txt") as f:
+        lines = f.readlines()
+
+    count = 0
+    matrix = []
+    for line in lines:
+        matrix.append([c for c in line.strip()])
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[0])):
+            count += int(is_xmas_right(matrix, i,j))
+            count += int(is_xmas_left(matrix, i,j))
+            count += int(is_xmas_down(matrix, i,j))
+            count += int(is_xmas_up(matrix, i,j))
+            count += int(is_xmas_right_up(matrix, i,j))
+            count += int(is_xmas_right_down(matrix, i,j))
+            count += int(is_xmas_left_up(matrix, i,j))
+            count += int(is_xmas_left_down(matrix, i,j))
+
+    return count
+
+
+def is_xmas_right(matrix: list[list[str]], i:int, j:int) -> bool:
+    if j > (len(matrix[0]) - 4):
+        return False
+    return matrix[i][j] == "X" and matrix[i][j+1] == "M" and matrix[i][j+2] == "A" and matrix[i][j+3] == "S"
+
+
+def is_xmas_left(matrix: list[list[str]], i:int, j:int) -> bool:
+    if j > (len(matrix[0]) - 4):
+        return False
+    return matrix[i][j] == "S" and matrix[i][j+1] == "A" and matrix[i][j+2] == "M" and matrix[i][j+3] == "X"
+
+
+def is_xmas_down(matrix: list[list[str]], i:int, j:int) -> bool:
+    if i > (len(matrix) - 4):
+        return False
+    return matrix[i][j] == "X" and matrix[i+1][j] == "M" and matrix[i+2][j] == "A" and matrix[i+3][j] == "S"
+
+
+def is_xmas_up(matrix: list[list[str]], i:int, j:int) -> bool:
+    if i > (len(matrix) - 4):
+        return False
+    return matrix[i][j] == "S" and matrix[i+1][j] == "A" and matrix[i+2][j] == "M" and matrix[i+3][j] == "X"
+
+
+def is_xmas_right_up(matrix: list[list[str]], i:int, j:int) -> bool:
+    if i > (len(matrix) - 4) or j > (len(matrix[0]) - 4):
+        return False
+    return matrix[i][j] == "X" and matrix[i+1][j+1] == "M" and matrix[i+2][j+2] == "A" and matrix[i+3][j+3] == "S"
+
+
+def is_xmas_left_down(matrix: list[list[str]], i:int, j:int) -> bool:
+    if i > (len(matrix) - 4) or j > (len(matrix[0]) - 4):
+        return False
+    return matrix[i][j] == "S" and matrix[i+1][j+1] == "A" and matrix[i+2][j+2] == "M" and matrix[i+3][j+3] == "X"
+
+
+def is_xmas_right_down(matrix: list[list[str]], i:int, j:int) -> bool:
+    if i < 3 or j > (len(matrix[0]) - 4):
+        return False
+    return matrix[i][j] == "X" and matrix[i-1][j+1] == "M" and matrix[i-2][j+2] == "A" and matrix[i-3][j+3] == "S"
+
+
+def is_xmas_left_up(matrix: list[list[str]], i:int, j:int) -> bool:
+    if i < 3 or j > (len(matrix[0]) - 4):
+        return False
+    return matrix[i][j] == "S" and matrix[i-1][j+1] == "A" and matrix[i-2][j+2] == "M" and matrix[i-3][j+3] == "X"
+          
+
+def second_part(instance: InstanceType, day: str):
+    with open(f"data/{instance.value}/{day}.txt") as f:
+        lines = f.readlines()
+
+    pass
+
+
+
+
+def main(instance: InstanceType):
+    day = os.path.basename(__file__).removesuffix(".py")
+
+    result = first_part(instance, day)
+    print(result)
+
+    # result = second_part(instance, day)
+    # print(result)
+
+
+if __name__ == "__main__":
+    typer.run(main)
